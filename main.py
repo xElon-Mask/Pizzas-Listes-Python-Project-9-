678f04ced601748bd422c92b9bd5a9be27c3bf27
xElon-Mask <xelonmask@protonmail.com>
2022-09-24 16:02:05 +0200

    create function afficher for print every pizzas in the terminal

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 pizzas = ("4 fromages", "Végétarienne", "Hawaï", "Calzone")
 
+def afficher(collection):
+    print("---- LISTE DES PIZZAS (", len(collection), ") ----")
+    for pizza in collection:
+        print(pizza)
+
+
+afficher(pizzas)
