7f53eabd7a41a0d15e9d3b5509a0bf893c764137
xElon-Mask <xelonmask@protonmail.com>
2022-09-24 16:16:06 +0200

    print 1st pizza, last pizza + print No Pizza if empty tuple

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 pizzas = ("4 fromages", "Végétarienne", "Hawaï", "Calzone")
+vide = ()
 
 def afficher(collection):
-    print("---- LISTE DES PIZZAS (", len(collection), ") ----")
-    for pizza in collection:
-        print(pizza)
+    if len(collection) == 0:
+         print("AUCUNE PIZZA")
+    else:
+        print("---- LISTE DES PIZZAS (", len(collection), ") ----")
+        for pizza in collection:
+            print(pizza)
+            # exo 1
+        print()
+    # afficher la 1ere pizza
+        print("Première Pizza : ", collection[0])
+    # afficher la dernière pizza
+        print("Dernière Pizza : ", collection[-1])
+
 
 
 afficher(pizzas)
+
+# exo 2 : protéger la fonction dans le cas où on passerait une collection vide
+# afficher alors "AUCUNE PIZZA"
