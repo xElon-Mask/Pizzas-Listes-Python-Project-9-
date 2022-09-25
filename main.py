96111cc011cb681bb994b1452d69f4d183c9a323
xElon-Mask <xelonmask@protonmail.com>
2022-09-25 12:05:17 +0200

    create function for user add a pizza

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 pizzas = ("4 fromages", "Végétarienne", "Hawaï", "Calzone")
-vide = ()
+# vide = ()
+
+def ajouter_pizza_utilisateur(collection):
+    pizza_utilisateur = input("Pizza à ajouter: ")
+    return collection, pizza_utilisateur
 
 def afficher(collection):
-    if len(collection) == 0:
+    maj_pizzas = ajouter_pizza_utilisateur(collection)
+    if len(maj_pizzas) == 0:
          print("AUCUNE PIZZA")
     else:
-        print("---- LISTE DES PIZZAS (", len(collection), ") ----")
-        for pizza in collection:
+        print("---- LISTE DES PIZZAS (", len(maj_pizzas) + len(collection) - 1, ") ----")
+        for pizza in maj_pizzas:
             print(pizza)
-            # exo 1
         print()
     # afficher la 1ere pizza
-        print("Première Pizza : ", collection[0])
+        print("Première Pizza : ", maj_pizzas[0][0])
     # afficher la dernière pizza
-        print("Dernière Pizza : ", collection[-1])
+        print("Dernière Pizza : ", maj_pizzas[-1])
 
 
 
-afficher(pizzas)
+
 
 # exo 2 : protéger la fonction dans le cas où on passerait une collection vide
 # afficher alors "AUCUNE PIZZA"
+
+#afficher(pizzas)
+# pizza à ajouter : l'user devra ajouter sa pizza. Il faudra alors qu'elle s'affiche à la fin de la collection
+# maj_pizzas = ajouter_pizza_utilisateur(pizzas)
+
+afficher(pizzas)
+
+
