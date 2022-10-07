5ce89710cfd0a87fb0c0e132951a6f7a61f3faeb
xElon-Mask <xelonmask@protonmail.com>
2022-10-07 11:44:53 +0200

    add option vegetarienne

--- pizzas_v2_poo.py
+++ pizzas_v2_poo.py
@@ -1,16 +1,21 @@
 # nom, prix, ingrédients, végétarienne
 
 class Pizza():
-    def __init__(self, nom, prix, ingredients):
+    def __init__(self, nom, prix, ingredients, vegetarienne=False):
         self.nom = nom
         self.prix = prix
         self.ingredients = ingredients
+        self.vegetarienne = vegetarienne
 
     def Afficher(self):
-        print(f"PIZZA {self.nom} : {self.prix} €")
+        if self.vegetarienne:
+            print(f"PIZZA {self.nom} : {self.prix}€ - VEGETARIENNE")
+        else:
+            print(f"PIZZA {self.nom} : {self.prix} €")
         print(", ".join(self.ingredients))
+        print()
 
-pizza1 = Pizza("4 fromages", 8.50, ("Brie", "Emmental", "Comté", "Parmesan"))
+pizza1 = Pizza("4 fromages", 8.50, ("Brie", "Emmental", "Comté", "Parmesan"), True)
 pizza1.Afficher()
 
 # rajouter plusieurs pizzas
@@ -19,7 +24,7 @@ pizza1.Afficher()
 # et il vous est également possible de donner un nombre différent d'ingrédients entre els différents pizzas
 
 pizzas = (Pizza("Tartiflette", 15.90, ("Reblochon", "Lardons", "Crème", "Oignons", "Vin Blanc")),
- Pizza("Végétarienne", 11.50, ("Poivrons", "Tomates cerises", "Emmental", "coeur d'artichaut")),
+ Pizza("Végétarienne", 11.50, ("Poivrons", "Tomates cerises", "Emmental", "coeur d'artichaut"), True),
  Pizza("Royale", 13.80, ("Salami", "Champigons", "Jambon")),
  Pizza("Océane", 17.60, ("Saumon", "Thon", "Moules", "Beurre Persillé"))
 )
@@ -29,3 +34,7 @@ pizzas = (Pizza("Tartiflette", 15.90, ("Reblochon", "Lardons", "Crème", "Oignon
 
 for pizza in pizzas:
     pizza.Afficher()
+
+# Ajouter une nouvelle fonctionnalité qui déclare si la pizza est végétarienne ou pas
+# on ajoute un paramètre optionnel, on dira par défaut qu'elle n'est pas végé
+# quand la pizza est végé il faudra l'indiquer au niveau de la fonction Afficher() : rajouter - VEGETARIENNE sur le 1er print
