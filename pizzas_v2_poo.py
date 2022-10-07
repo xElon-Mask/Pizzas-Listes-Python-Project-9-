962ff2ef37bad6664aaaecd18dab5dffd4ce3516
xElon-Mask <xelonmask@protonmail.com>
2022-10-07 10:59:38 +0200

    add ingredients parameters

--- pizzas_v2_poo.py
+++ pizzas_v2_poo.py
@@ -1,12 +1,14 @@
 # nom, prix, ingrédients, végétarienne
 
 class Pizza():
-    def __init__(self, nom, prix):
+    def __init__(self, nom, prix, ingredients):
         self.nom = nom
         self.prix = prix
+        self.ingredients = ingredients
 
     def Afficher(self):
         print(f"PIZZA {self.nom} : {self.prix} €")
+        print(", ".join(self.ingredients))
 
-pizza1 = Pizza("4 fromages", 8.50)
+pizza1 = Pizza("4 fromages", 8.50, ("Brie", "Emmental", "Comté", "Parmesan"))
 pizza1.Afficher()
