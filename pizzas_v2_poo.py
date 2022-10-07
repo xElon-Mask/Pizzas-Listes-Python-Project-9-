e41a5fc34c5a9ce381d446fad909df8c3dcd64be
xElon-Mask <xelonmask@protonmail.com>
2022-10-07 11:18:20 +0200

    create tuple of pizzas + loop to print every pizza

--- pizzas_v2_poo.py
+++ pizzas_v2_poo.py
@@ -12,3 +12,20 @@ class Pizza():
 
 pizza1 = Pizza("4 fromages", 8.50, ("Brie", "Emmental", "Comté", "Parmesan"))
 pizza1.Afficher()
+
+# rajouter plusieurs pizzas
+# avec une nouvelle variable pizzas qui va être un tuple de plusieurs pizzas, 3 ou 4
+# chaque pizza aura un nom différent, un prix différent, et aura des ingédients différents
+# et il vous est également possible de donner un nombre différent d'ingrédients entre els différents pizzas
+
+pizzas = (Pizza("Tartiflette", 15.90, ("Reblochon", "Lardons", "Crème", "Oignons", "Vin Blanc")),
+ Pizza("Végétarienne", 11.50, ("Poivrons", "Tomates cerises", "Emmental", "coeur d'artichaut")),
+ Pizza("Royale", 13.80, ("Salami", "Champigons", "Jambon")),
+ Pizza("Océane", 17.60, ("Saumon", "Thon", "Moules", "Beurre Persillé"))
+)
+
+
+# Utiliser une boucle pour afficher les différentes pizzas 
+
+for pizza in pizzas:
+    pizza.Afficher()
